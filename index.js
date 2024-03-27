2600696b097d756b32a3fb3f790dd90b0cd93584
tyranorah <tashnorah@gmail.com>
2024-03-27 22:50:08 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+// Define function expression called divide
+function divide() {
+    return 2000 / 100;
+}
+
+// Define arrow function called square
+const square = (num) => num * num;
+
+// Define arrow function called add
+const add = (num1, num2) => num1 + num2;
+
